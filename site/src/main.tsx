b166994652d581f4d608068050353730772a0f88
mryk814 <54519966+mryk814@users.noreply.github.com>
2026-07-16 13:11:15 +0900

    style: load supplemental UX styles once at the app entry

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+import "./components/PageOrientation.css";
+import "./features/methods/MethodPage.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
